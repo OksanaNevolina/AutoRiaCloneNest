5c11c19ea91fcc9f90350a31e9f670544c41a1ae
OksanaNevolina <onevolina5gmail.com>
2024-04-23 22:24:39 +0300

    update userEntity

--- src/database/entities/user.entity.ts
+++ src/database/entities/user.entity.ts
@@ -1,8 +1,9 @@
 import { Column, Entity, OneToMany } from 'typeorm';
-
 import { TableNameEnum } from '../enums/table-name.enum';
 import { BaseEntity } from './models/base.entity';
 import { RefreshTokenEntity } from './refresh-token.entity';
+import {AccountTypeEnum} from "../enums/account-type.enum";
+import {RoleEnum} from "../enums/role-enum";
 
 @Entity(TableNameEnum.USERS)
 export class UserEntity extends BaseEntity {
@@ -14,6 +15,24 @@ export class UserEntity extends BaseEntity {
 
   @Column('text', { select: false })
   password: string;
+
+  @Column('boolean', { default: true })
+  status: boolean;
+
+  @Column({
+    type: "enum",
+    enum: AccountTypeEnum,
+    default: AccountTypeEnum.BASE,
+  })
+  accountType: AccountTypeEnum
+
+  @Column({
+    type: "enum",
+    enum: RoleEnum,
+    default: RoleEnum.SELLER,
+  })
+  role: RoleEnum
+
   @OneToMany(() => RefreshTokenEntity, (entity) => entity.user)
   refreshTokens?: RefreshTokenEntity[];
 }
