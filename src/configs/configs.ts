cda255b041d6c83f7f465f8928356d61f9963400
OksanaNevolina <onevolina5gmail.com>
2024-05-10 22:11:03 +0300

    add git+change configs

--- src/configs/configs.ts
+++ src/configs/configs.ts
@@ -50,23 +50,23 @@ export default (): Config => ({
   },
   smtp:{
     userSmtp:
-    process.env.SMTP_USER || 'onevolina5@gmail.com',
+    process.env.SMTP_USER ,
     passSmtp:
-    process.env.SMTP_PASSWORD || 'eohdbdkkzqkkvebf'
+    process.env.SMTP_PASSWORD
   },
   frontURL:{
     frontURL: process.env.FRONT_URL,
   },
   AWSs3:{
     awsS3AccessKey:
-        process.env.AWS_S3_ACCESS_KEY || "AKIAWLWQMW4RZU3U47GB",
+        process.env.AWS_S3_ACCESS_KEY,
     awsS3SecretKey:
-        process.env.AWS_S3_SECRET_KEY || "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",
+        process.env.AWS_S3_SECRET_KEY,
     awsS3BucketName:
-    process.env.AWS_S3_BUCKET_NAME || "bucket-f-avka",
+    process.env.AWS_S3_BUCKET_NAME,
     awsS3Region:
-        process.env.AWS_S3_REGION || "us-east-1",
+        process.env.AWS_S3_REGION,
     awsS3URL:
-        process.env.AWS_S3_URL || "https://bucket-f-avka.s3.amazonaws.com/"
+        process.env.AWS_S3_URL
   }
 });
